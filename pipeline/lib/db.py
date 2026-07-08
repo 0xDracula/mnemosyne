52fdae911ab9d366d97bdc1f25f565168e6134a3
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-07-08 19:05:40 +0300

    infra: role passwords from env

--- pipeline/lib/db.py
+++ pipeline/lib/db.py
@@ -4,4 +4,12 @@ import psycopg
 
 
 def connect(dsn: str | None = None) -> psycopg.Connection:
-    return psycopg.connect(dsn or os.environ["PIPELINE_DATABASE_URL"])
+    if dsn is not None:
+        return psycopg.connect(dsn)
+    return psycopg.connect(
+        host=os.environ["POSTGRES_HOST"],
+        port=os.environ["POSTGRES_PORT"],
+        dbname=os.environ["POSTGRES_DB"],
+        user=os.environ["PIPELINE_DB_USER"],
+        password=os.environ["PIPELINE_DB_PASSWORD"],
+    )
